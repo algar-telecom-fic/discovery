2b0b0426fd5ffd2508b2a8b939b03a3b648f0a9f
ggardusi <gustavo.gardusi@gmail.com>
2019-02-13 16:07:28 -0200

    .

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Juniper(Router):
   manufacturer = 'Juniper'
 
   def valid(self, credentials):
-    return super().valid(credentials, 'JUNOS')
+    return self.super().valid(credentials, 'JUNOS')
 
 class Cisco_XR(Router):
   manufacturer = 'Cisco-XR'
